21e6ebdb0156b8b965cdb06ace7668e64384b635
KaiHDev <kaihallam95@gmail.com>
2022-10-28 12:13:37 +0100

    Api Update

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
         baseQuery: fetchBaseQuery({
             baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
             prepareHeaders: (headers) => {
-                headers.set('X-RapidAPI-Key', 'b592bf9e36msha21994b145d11b5p14bbb0jsn344c453fc60c');
+                headers.set('X-RapidAPI-Key', 'e93d979335msh4f8787366b82fa0p14fa1cjsn1f2066a2593d');
             
                 return headers;
             },
